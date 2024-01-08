714623bcc8089086230ac7d11d66782d3ae25b95
LeonardoMelloSilvestri <leonardomscontato@gmail.com>
2024-01-08 14:19:27 -0300

    Adding another project called Space Wars 2, solving some responsibility issues, adding name, description and tools for each of the projects.

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -1,13 +1,16 @@
-import topCars from "../assets/img/TopCars.png";
+import topCars from "../assets/img/topCars.jpg";
 import pandaExpress from "../assets/img/PandaExpress.jpg";
 import wwii from "../assets/img/wwii.jpg";
+import spaceWars2 from "../assets/img/spaceWars2.jpg";
 export default function Projetos() {
   const projetos = [
     {
       id: 1,
       name: "Top Cars",
+      theme: "Vendas",
       description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero odio impedit recusandae nobis.",
+        "Landing Page desenvolvida para uma concessionária de carros.",
+      tools: ["React", "Tailwind"],
       url: "https://LeonardoMelloSilvestri.github.io/TopCars",
       img: topCars,
     },
@@ -15,7 +18,8 @@ export default function Projetos() {
       id: 2,
       name: "Panda Express",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptatum nulla queue.",
+        "Landing Page desenvolvida para uma amiga, dona de um restaurante de fast food localizado na Venezuela.",
+      tools: ["React", "Tailwind"],
       url: "https://LeonardoMelloSilvestri.github.io/PandaExpress",
       img: pandaExpress,
     },
@@ -23,9 +27,19 @@ export default function Projetos() {
       id: 3,
       name: "World War II",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quas minus use.",
+        "Projeto pessoal com temática da Segunda Guerra Mundial. Possui o intuíto de ensinar a história desse conflito tão importante.",
+      tools: ["React", "Chakra UI", "Zustand"],
       url: "https://LeonardoMelloSilvestri.github.io/WorldWarII",
       img: wwii,
+    },
+    {
+      id: 4,
+      name: "Space Wars 2",
+      description:
+        "Jogo web de batalha espacial, fácil de jogar e muito divertido.",
+      tools: ["HTML - Canvas", "Javascript"],
+      url: "https://LeonardoMelloSilvestri.github.io/SpaceWars2",
+      img: spaceWars2,
     },
   ];
 
@@ -33,22 +47,34 @@ export default function Projetos() {
     <>
       <article
         id="projetos"
-        className="flex flex-col items-center gap-8 mt-24 px-8 md:px-24"
+        className="flex flex-col items-center gap-8 mt-24 px-8 lg:px-14"
       >
         <h1 className="text-3xl dark:text-bone-100 font-semibold">Projetos</h1>
-        <div className="flex flex-wrap gap-6 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 xl:gap-4 w-full">
           {projetos.map((projeto) => (
-            <a
+            <div
               key={projeto.id}
-              href={projeto.url}
-              target="__blank"
-              className="flex flex-col justify-center cursor-pointer items-center flex-auto w-80 h-60"
+              className="flex flex-col text-center dark:text-bone-100 gap-3"
             >
-              <img
-                className="object-fill h-full w-full border-indigo-600 border-2 border-solid border-sm rounded-2xl"
-                src={projeto.img}
-              />
-            </a>
+              <a href={projeto.url} target="__blank" className="h-64">
+                <img
+                  className="object-fill h-full w-full border-indigo-600 border-2 border-solid border-sm rounded-2xl"
+                  src={projeto.img}
+                />
+              </a>
+              <h1 className="text-xl font-semibold">{projeto.name}</h1>
+              <p className="">{projeto.description}</p>
+              <div className="flex gap-2 justify-center">
+                {projeto.tools.map((tool, index) => (
+                  <span
+                    key={index}
+                    className="rounded-md bg-indigo-500 px-2 py-1 text-sm font-medium text-white ring-1 ring-inset ring-indigo-600"
+                  >
+                    {tool}
+                  </span>
+                ))}
+              </div>
+            </div>
           ))}
         </div>
       </article>
